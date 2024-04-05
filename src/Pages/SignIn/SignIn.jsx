ce9562869657478ff39917e8d7b170174af3d7f2
yogesh <yogesh@locad.net>
2024-04-05 08:06:06 +0530

    background Image for Log in and Sign up

--- src/Pages/SignIn/SignIn.jsx
+++ src/Pages/SignIn/SignIn.jsx
@@ -37,7 +37,15 @@ const SignIn = () => {
   };
 
   return (
-    <div className="h-screen w-screen flex justify-center items-center">
+    <div
+      className="h-screen w-screen flex justify-center items-center"
+      style={{
+        backgroundImage:
+          "URL('https://images2.alphacoders.com/132/1325574.png')",
+          backgroundRepeat:"no-repeat",
+          backgroundSize:"cover"
+      }}
+      >
       <div className="h-96 w-96 border-2 flex flex-col justify-center items-center bg-slate-100 rounded-xl">
         <img src="/Logos/amazonLogo.png" className="h-16" />
         <div className="flex flex-col h-2/5 w-full justify-center items-center">
--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -54,7 +54,14 @@ const SignUp = () => {
   };
 
   return (
-    <div className="h-screen w-screen flex justify-center items-center">
+    <div
+      className="h-screen w-screen flex justify-center items-center"
+      style={{
+        backgroundImage:
+          "URL('https://images2.alphacoders.com/132/1325574.png')",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}>
       <div className="h-96 w-96 border-2 flex flex-col justify-evenly items-center bg-slate-100 rounded-xl">
         <img src="/Logos/amazonLogo.png" className="h-16 mt-5" />
         <div className="flex flex-col h-2/3 w-full justify-center items-center">
